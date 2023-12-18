983793e90a8d85bc17a7a37268f8b9f61f8b4f92
to_binio <tobias.frischmann1@gmail.com>
2023-12-18 11:26:33 +0100

    add eslint & prettier add github workflow

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import express from "express";
 const port = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("moin");
-})
+app.get("/", (_req, res) => {
+  res.send("moin");
+});
 
 app.listen(port, () => {
-    console.log("backend running on port: " + port);
-})
+  console.log("backend running on port: " + port);
+});
