4aa3c464f2495de80cdcc22df0b509b824d171f3
benni043 <benni.huff@gmail.com>
2023-12-20 23:02:06 +0100

    changed index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import { client } from "./db/dbConfig";
+import cors from "cors";
+import "dotenv/config";
+import { getExampleRouter } from "./example/example.router";
 
-const port = 3000;
-let app = express();
+const port = parseInt(process.env.BACKEND_PORT as string);
+const server = express();
 
-app.get("/", (_req, res) => {
-  res.send("moin");
-});
+//enable json in body
+server.use(express.json());
 
-app.listen(port, () => {
+//enable cors
+server.use(cors());
+
+//add routers
+server.use("/api/example/", getExampleRouter());
+
+server.listen(port, async () => {
+  await client.connect();
   console.log("backend running on port: " + port);
 });
