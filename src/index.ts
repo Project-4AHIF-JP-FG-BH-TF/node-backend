749f2ae2f35ecaba60ef67552a0a914ec9debd7b
benni <benni.huff@gmail.com>
2023-12-20 13:49:31 +0100

    basic db implementation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import {client} from "./config";
 
 const port = 3000;
 const app = express();
@@ -7,6 +8,21 @@ app.get("/", (req, res) => {
     res.send("moin");
 })
 
-app.listen(port, () => {
+app.get("/person/", async (req, res) => {
+    let query = `
+        SELECT *
+        FROM person
+    `
+
+    const result = await client.query(query);
+    await client.end();
+
+    res.send(result.rows)
+    res.end();
+})
+
+app.listen(port, async () => {
     console.log("backend running on port: " + port);
+
+    await client.connect();
 })
