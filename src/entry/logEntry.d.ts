00e17f892aee5e3629911e0883d2a5fdb9d81d2f
benni043 <benni.huff@gmail.com>
2024-01-06 20:42:34 +0100

    created files logEntry type init

---
+++ src/entry/logEntry.d.ts
@@ -0,0 +1,19 @@
+import {UUID} from "node:crypto";
+
+export type LogEntry = {
+    session_id: UUID,
+    file_name: string,
+    entry_nr: number,
+    creation_date: Date,
+    classification: Classification,
+    service_ip: string,
+    user_id: string,
+    user_session_id: string,
+    java_class: string,
+    content: string
+}
+
+export enum Classification {
+    info,
+    error
+}
