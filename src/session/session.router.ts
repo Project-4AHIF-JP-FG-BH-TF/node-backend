5f9009aafd428fc8d3c0e80695b871c341e610aa
tobinio <Tobias.frischmann1@gmail.com>
2024-01-06 14:16:00 +0100

    add session refresh

--- src/session/session.router.ts
+++ src/session/session.router.ts
@@ -1,3 +1,4 @@
+import { UUID } from "node:crypto";
 import { Router } from "express";
 import { SessionService } from "./session.service";
 
@@ -6,6 +7,18 @@ export function getSessionRouter(): Router {
 
   router.get("/", async (_req, res) => {
     const uuid = await SessionService.getInstance().createNew();
+
+    if (uuid) {
+      res.status(200).json({ uuid }).end();
+    } else {
+      res.status(500).end();
+    }
+  });
+
+  router.post("/:id", async (req, res) => {
+    const uuid = await SessionService.getInstance().refresh(
+      req.params.id as UUID,
+    );
 
     if (uuid) {
       res.status(200).json({ uuid }).end();
